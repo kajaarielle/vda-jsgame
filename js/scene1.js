703d629e363fad1e004d3a2849d9123515359e19
kaja arielle <47561841+kajaarielle@users.noreply.github.com>
2022-05-02 17:35:54 +0300

    random enemy moves

--- js/scene1.js
+++ js/scene1.js
@@ -58,14 +58,14 @@ class Scene1 extends Phaser.Scene {
 
     //#region PLAYER INPUT CONTROLLER
     // Basic move button
-    // this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-    // this.keyUp.on("down", this.whenKeyUPPressed, this);
-    // this.keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-    // this.keyDown.on("down", this.whenKeyDOWNPressed, this);
-    // this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-    // this.keyLeft.on("down", this.whenKeyLEFTPressed, this);
-    // this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    // this.keyRight.on("down", this.whenKeyRIGHTPressed, this);
+    this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.keyUp.on("down", this.whenKeyUPPressed, this);
+    this.keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    this.keyDown.on("down", this.whenKeyDOWNPressed, this);
+    this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    this.keyLeft.on("down", this.whenKeyLEFTPressed, this);
+    this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    this.keyRight.on("down", this.whenKeyRIGHTPressed, this);
     //#endregion
 
     //#region RENDER TILEMAP
@@ -92,24 +92,49 @@ class Scene1 extends Phaser.Scene {
       this.spawn(7, 2), // y
       "dinosaur"
     );
+    this.dinosaurSprite2 = this.add.sprite(
+      this.spawn(10, 12), // x
+      this.spawn(10, 2), // y
+      "dinosaur"
+    );
     //#endregion
-    
+
     //#region GRID ENGINE CONFIG
-    const gridEngineConfig = {
+    this.gridEngineConfig = {
       characters: [
         {
           id: "player",
           sprite: this.playerSprite,
           //walkingAnimationMapping: ,
           startPosition: { x: 1, y: 2 },
+          attackMeleeDMG: 3,
+          attackRange: 3,
+          attackRangeDMG: 2,
+        },
+        {
+          id: "dinosaur",
+          sprite: this.dinosaurSprite,
+          startPosition: { x: 6, y: 8 },
+          attackMeleeDMG: 2,
+          attackRange: 2,
+          attackRangeDMG: 1,
+        },
+        {
+          id: "dinosaur2",
+          sprite: this.dinosaurSprite2,
+          startPosition: { x: 8, y: 10 },
+          attackMeleeDMG: 2,
+          attackRange: 2,
+          attackRangeDMG: 1,
         },
       ],
     };
+    this.enemyNumbers = Object.keys(this.gridEngineConfig.characters).length - 1;
+    //this.enemies = ["dinosaur", "dinosaur2"];
 
-    this.gridEngine.create(tileMap, gridEngineConfig);
+
+    this.gridEngine.create(tileMap, this.gridEngineConfig);
     //#endregion
-
-
 
     //this.player.setCollideWorldBounds(true);
 
@@ -121,7 +146,7 @@ class Scene1 extends Phaser.Scene {
     this.playerTurnsUI = this.add.text(0, 0, 'Player turns: ' + this.playerTurns);
     this.testUI = this.add.text(0, 24, 'ATTACK');
     //#endregion
-    
+
     //#region DEBUG MODE
     if (this.debugMode) {
       this.drawGrid();
@@ -130,46 +155,14 @@ class Scene1 extends Phaser.Scene {
   }
 
   update() {
-    const cursors = this.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown) {
-      this.gridEngine.move("player", "left");
-    } else if (cursors.right.isDown) {
-      this.gridEngine.move("player", "right");
-    } else if (cursors.up.isDown) {
-      this.gridEngine.move("player", "up");
-    } else if (cursors.down.isDown) {
-      this.gridEngine.move("player", "down");
-    }
-  
-
     //#region TEST PLAYER MOVEMENT
-    // var distance = Phaser.Math.Distance.Between(this.player.x, this.player.y, this.target.x, this.target.y);
-
-    // if (this.player.body.speed > 0) {
-    //   console.log("The player is moving!");
-    //   //distanceText.setText('Distance: ' + distance);
-
-    //   //  4 is our distance tolerance, i.e. how close the source can get to the target
-    //   //  before it is considered as being there. The faster it moves, the more tolerance is required.
-    //   if (distance < 4) {
-    //     this.player.body.reset(this.target.x, this.target.y);
-    //     console.log("Player destination reached.");
-    //   }
-    // }
-
-    // if i need to check for something every frame
-
 
     // var isUpDown = this.input.keyboard.checkDown(this.controller.keyUp)
     // if (isUpDown) {
     //   if (this.playerTurn && this.playerTurns != 0) {
     //     // move
 
-    //     this.playerTurns--;
-    //     this.setTextUI();
-    //     if (this.playerTurns == 0) {
-    //       this.endPlayerTurn();
-    //     }
+
     //   }
     // }
 
@@ -209,44 +202,150 @@ class Scene1 extends Phaser.Scene {
     //   }
     // }
     //#endregion
-
-
-  }
-  surroundingPlayerTiles() {
-    let x = this.player.x;
-    let y = this.player.y;
   }
 
-  setTextUI() {
-    this.playerTurnsUI.setText('Player turns: ' + this.playerTurns);
-  }
+  //#region CUSTOM FUNCTIONS
 
-  visibilityUI(nameUI, visibility) {
-    let visible = visibility;
-    this.setThisUI = nameUI;
-
-    if (visible == true) {
-      this.setThisUI.visible = true;
-    }
-    else if (visible == false) {
-      this.setThisUI.visible = false;
+  //#region CONTROLLER FUNCTIONS
+  whenKeyUPPressed() {
+    if (this.playerTurn && this.playerTurns != 0) {
+      this.gridEngine.move("player", "up");
+      // check if the player moved or not during their turn
+      this.playerMoveFinished();
     }
   }
+  whenKeyDOWNPressed() {
+    if (this.playerTurn && this.playerTurns != 0) {
+      this.gridEngine.move("player", "down");
+      this.playerMoveFinished();
+    }
 
+  }
+  whenKeyLEFTPressed() {
+    if (this.playerTurn && this.playerTurns != 0) {
+      this.gridEngine.move("player", "left");
+      //this.visibilityUI(this.testUI, false);
+
+      this.playerMoveFinished();
+    }
+
+  }
+  whenKeyRIGHTPressed() {
+    if (this.playerTurn && this.playerTurns != 0) {
+      this.gridEngine.move("player", "right");
+      this.playerMoveFinished();
+    }
+  }
+  //#endregion
+  whenKeyQPressed() {
+    // do something
+    var canMove = this.playerMoveFreely;
+
+    if (canMove) this.playerMoveFreely = false;
+    else this.playerMoveFreely = true;
+
+    console.log(this.playerMoveFreely);
+  }
+  //#region TURNBASED
+
+  // Called after every player move.
+  playerMoveFinished() {
+    this.playerTurns--;
+    this.setTextUI();
+    if (this.playerTurns == 0) {
+      // If no more turns, end player turn.
+      this.endPlayerTurn();
+    }
+  }
+
+  // Called when the player has finished all their turns
   endPlayerTurn() {
     console.log("Player turn over");
     this.enemyTurn();
   }
 
+  // Called after player turn, before resetting max player turns
   enemyTurn() {
     var enemyTurnDone = false;
+    //this.gridEngine.
     // loop every enemy array and make them run their turn
     // after all enemies are done with their moves set enemyTurnDone = true;
+    // for (let i = 0; i < this.enemyNumbers; i++) {
+    //   this.enemyDoSomething(this.gridEngineConfig.characters[i]);
+    // }
+    let myArray = this.gridEngineConfig.characters;
+
+    myArray.forEach(element => {
+      if(element.id != "player") {
+        this.currentEnemy = element;
+      this.enemyDoSomething(element)
+      }
+    });
+
+
+    //this.enemyDoSomething("dinosaur");
+
     enemyTurnDone = true;
 
     if (enemyTurnDone) {
       this.resetPlayerTurn();
     }
+  }
+  enemyDoSomething(enemy) {
+    console.log(enemy);
+
+    // 1. check if the enemy is in range to player
+    // 2. if they are, it is possible to do attack (range vs melee?)
+    // 3. select randomMoveInt based on moves possible
+    let movesPossible = 0;
+
+    let canAttackRange = this.checkRangeOverlap(enemy, this.currentEnemy.attackRange);
+    let canAttackMelee = this.checkRangeOverlap(enemy, 1);
+    let canMove = true;
+
+    if (canMove)
+      movesPossible++;
+    if (canAttackRange)
+      movesPossible++;
+    if (canAttackMelee)
+      movesPossible++;
+
+    let randomMove = this.getRandomIntFromMax(movesPossible);
+
+    if (randomMove == 0) {
+      console.log("Character should do a move now");
+      //move
+      // check if the character can move in x direction, if they can, make it possible move
+
+      switch (this.getRandomIntFromMax(4)) {
+        case 0:
+          this.gridEngine.move(this.currentEnemy.id, "right");
+          break;
+        case 1:
+          this.gridEngine.move(this.currentEnemy.id, "left");
+          break;
+        case 2:
+          this.gridEngine.move(this.currentEnemy.id, "up");
+          break;
+        case 3:
+          this.gridEngine.move(this.currentEnemy.id, "down");
+          break;
+      }
+      console.log("This is after said character move");
+    }
+    else if (randomMove == 1) {
+      //attack range
+    }
+
+    else if (randomMove == 2) {
+      // attack melee
+    }
+  }
+  checkRangeOverlap(character, range) {
+    // check overlap range, use character and range
+    let rangeOverlap = false;
+
+    return rangeOverlap;
   }
 
   resetPlayerTurn() {
@@ -255,48 +354,10 @@ class Scene1 extends Phaser.Scene {
     this.setTextUI();
   }
 
-  //#region OLD CONTROLLER FUNCTIONS
-  // whenKeyUPPressed() {
-  //   if (this.playerTurn && this.playerTurns != 0) {
-  //     // move player to target at set speed (px/s)
-  //     this.target.x = this.player.x;
-  //     this.physics.moveTo(this.player, this.target.x, this.player.y, this.moveSpeed);
-
-  //     this.playerMoveFinished();
-  //   }
-  // }
-  // whenKeyDOWNPressed() {
-  //   if (this.playerTurn && this.playerTurns != 0) {
-  //     // move
-  //     this.playerMoveFinished();
-  //   }
-
-  // }
-  // whenKeyLEFTPressed() {
-  //   if (this.playerTurn && this.playerTurns != 0) {
-  //     // move
-  //     this.visibilityUI(this.testUI, false);
-
-  //     this.playerMoveFinished();
-  //   }
-
-  // }
-  // whenKeyRIGHTPressed() {
-  //   if (this.playerTurn && this.playerTurns != 0) {
-  //     // move
-  //     this.playerMoveFinished();
-  //   }
-  // }
-  //#endregion
-
-  playerMoveFinished() {
-    this.playerTurns--;
-    this.setTextUI();
-    if (this.playerTurns == 0) {
-      this.endPlayerTurn();
-    }
+  surroundingPlayerTiles() {
+    let x = this.player.x;
+    let y = this.player.y;
   }
-
 
   // if this.playerTurns != 0
   // player can do one of x
@@ -325,24 +386,44 @@ class Scene1 extends Phaser.Scene {
   //   this.playerTurns = this.playerTurnsMax;
   //   console.log("Reset turns");
   // }
-  whenKeyQPressed() {
-    // do something
-    var canMove = this.playerMoveFreely;
+  //#endregion
 
-    if (canMove) this.playerMoveFreely = false;
-    else this.playerMoveFreely = true;
-
-    console.log(this.playerMoveFreely);
+  //#region UI
+  setTextUI() {
+    this.playerTurnsUI.setText('Player turns: ' + this.playerTurns);
   }
 
-  drawGrid() {
-    // https://phaser.io/examples/v3/view/game-objects/shapes/grid
-    var g1 = this.add.grid(128, 128, 256, 256, 16, 16, 0xff0000, 0.2);
-  }
+  visibilityUI(nameUI, visibility) {
+    let visible = visibility;
+    this.setThisUI = nameUI;
 
+    if (visible == true) {
+      this.setThisUI.visible = true;
+    }
+    else if (visible == false) {
+      this.setThisUI.visible = false;
+    }
+  }
+  //#endregion
+
+  //#region OTHER FUNCTIONS
   spawn(gridPlacement, offset) {
     const gridSize = 16;
     const place = gridPlacement * gridSize - offset;
     return place;
   }
+  getRandomIntFromMax(max) {
+    return Math.floor(Math.random() * max);
+  }
+  //#endregion
+
+  //#region DEBUG
+  drawGrid() {
+    // https://phaser.io/examples/v3/view/game-objects/shapes/grid
+    var g1 = this.add.grid(128, 128, 256, 256, 16, 16, 0xff0000, 0.2);
+  }
+  //#endregion
+
+  //#endregion
 }
+
