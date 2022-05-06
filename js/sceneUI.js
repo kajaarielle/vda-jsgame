8b8dae7f44d52db934dda292adb23502798aa06d
kajaarielle <47561841+kajaarielle@users.noreply.github.com>
2022-05-06 21:27:23 +0300

    temporary disabled new ui

--- js/sceneUI.js
+++ js/sceneUI.js
@@ -8,23 +8,25 @@ class SceneUI extends Phaser.Scene {
   }
 
   create() {
+    this.uiEnabled = false;
+
     //  Check the Registry and hit our callback every time the 'score' value is updated
-    this.registry.events.on('changedata', this.updateData, this);
+    if (this.uiEnabled) {
+      this.registry.events.on('changedata', this.updateData, this);
 
-    this.turnsText = this.add.text(10, 48, 'Turns: x', { font: '32px Arial', fill: '#000000' });
-    this.healthText = this.add.text(10, 10, 'Health: x', { font: '32px Arial', fill: '#000000' });
+      this.turnsText = this.add.text(10, 48, 'Turns: x', { font: '32px Arial', fill: '#000000' });
+      this.healthText = this.add.text(10, 10, 'Health: x', { font: '32px Arial', fill: '#000000' });
 
-  }
-  updateData (parent, key, data)
-    {
-        if (key === 'playerHealth')
-        {
-            this.healthText.setText('Health: ' + data);
-        }
-        else if (key === 'playerTurns')
-        {
-            this.turnsText.setText('Turns: ' + data);
-        }
     }
+  }
+  updateData(parent, key, data)
+    {
+      if (key === 'playerHealth' && this.uiEnabled) {
+        this.healthText.setText('Health: ' + data);
+      }
+      else if (key === 'playerTurns' && this.uiEnabled) {
+        this.turnsText.setText('Turns: ' + data);
+      }
+  }
 }
 
